324c4b4fe94d11ba88a932a2855f1d2afb777bfa
luoyong <luoyongsir@gmail.com>
2019-06-04 22:09:00 +0800

    fix name

--- demo/demo-service/src/test/java/com/nice/sboot/demo/service/BaseTest.java
+++ demo/demo-service/src/test/java/com/nice/sboot/demo/service/BaseTest.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
- * @author luoyong-014
+ * @author luoyong
  * @date 2018/8/1 16:48
  */
 @RunWith(SpringJUnit4ClassRunner.class)
