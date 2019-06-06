a052fefb0271f6278cbfb05490b1805b3edc98c2
罗勇 <luoyong@13322ty.com>
2019-06-06 17:37:27 +0800

    fix

--- demo/demo-service/src/test/java/com/nice/sboot/demo/service/BaseTest.java
+++ demo/demo-service/src/test/java/com/nice/sboot/demo/service/BaseTest.java
@@ -1,18 +1,17 @@
 package com.nice.sboot.demo.service;
 
-import com.nice.sboot.demo.BootDemoStrap;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author luoyong
  * @date 2018/8/1 16:48
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = BootDemoStrap.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class BaseTest {
     protected static final Logger LOG = LoggerFactory.getLogger(BaseTest.class.getName());
 }
