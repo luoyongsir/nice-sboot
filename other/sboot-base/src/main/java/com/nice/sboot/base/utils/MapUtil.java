9077e5d5c1e21a4ee37aa06b59664cc673bfe2cb
罗勇 <luoyong@13322ty.com>
2019-06-20 10:21:41 +0800

    参考 guava maps

--- other/sboot-base/src/main/java/com/nice/sboot/base/utils/MapUtil.java
+++ other/sboot-base/src/main/java/com/nice/sboot/base/utils/MapUtil.java
@@ -1,9 +1,12 @@
 package com.nice.sboot.base.utils;
 
+import com.google.common.primitives.Ints;
+
 import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * see com.google.common.collect.Maps
  * @author luoyong
  * @date 2019/6/19 18:34
  */
@@ -27,7 +30,7 @@ public final class MapUtil {
 	 * 根据等号左边的类型, 构造类型正确的HashMap.
 	 *
 	 */
-	public static <K, V> Map<K, V> newHashMap() {
+	public static <K, V> HashMap<K, V> newHashMap() {
 		return new HashMap<>();
 	}
 
@@ -35,7 +38,7 @@ public final class MapUtil {
 	 * 根据等号左边的类型和key的预期size, 构造类型正确的HashMap.
 	 *
 	 */
-	public static <K, V> Map<K, V> newHashMap(int expectedSize) {
+	public static <K, V> HashMap<K, V> newHashMap(int expectedSize) {
 		return newHashMap(expectedSize, 0.75F);
 	}
 
@@ -47,9 +50,19 @@ public final class MapUtil {
 	 * 加载因子也是HashMap中减少Hash冲突的重要一环，如果读写频繁，总记录数不多的Map，可以比默认值0.75进一步降低，建议0.5
 	 *
 	 */
-	public static <K, V> Map<K, V> newHashMap(int expectedSize, float loadFactor) {
-		int initialCapacity = (int) ((double) expectedSize / loadFactor + 1.0F);
+	public static <K, V> HashMap<K, V> newHashMap(int expectedSize, float loadFactor) {
+		int initialCapacity = capacity(expectedSize, loadFactor);
 		return new HashMap<>(initialCapacity, loadFactor);
+	}
+
+	private static int capacity(int expectedSize, float loadFactor) {
+		if (expectedSize < 3) {
+			return 4;
+		}
+		if (expectedSize < Ints.MAX_POWER_OF_TWO) {
+			return (int) ((float) expectedSize / loadFactor + 1.0F);
+		}
+		return Integer.MAX_VALUE;
 	}
 
 	private MapUtil() {
