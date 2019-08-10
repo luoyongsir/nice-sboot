31361cefab1f9bc5409b474075df6d8a7ce5e76f
luoyong <luoyongsir@mail.com>
2019-08-10 22:22:58 +0800

    添加 服务网关聚合swagger接口

---
+++ cloud/gateway-web-swagger/src/main/java/com/nice/sboot/gateway/swagger/BootGatewaySwaggerWeb.java
@@ -0,0 +1,17 @@
+package com.nice.sboot.gateway.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luoyong
+ * @date 2019/8/10 22:16
+ */
+@SpringBootApplication
+public class BootGatewaySwaggerWeb {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootGatewaySwaggerWeb.class, args);
+	}
+
+}
