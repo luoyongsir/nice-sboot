9d50985f0a441659791a44597552beefea2bab02
罗勇 <luoyongsir@gmail.com>
2020-06-09 00:18:09 +0800

    去掉mongodb，去掉swagger2，改用springdoc，dubbo序列化改成kryo

--- demo/demo-web/src/main/java/com/nice/sboot/demo/web/config/SpringMvcConfig.java
+++ demo/demo-web/src/main/java/com/nice/sboot/demo/web/config/SpringMvcConfig.java
@@ -1,13 +1,7 @@
 package com.nice.sboot.demo.web.config;
 
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 /**
  * @author luoyong
@@ -16,16 +10,16 @@ import java.util.List;
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
-	@Override
-	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-		FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
-
-		// fast json 循环引用
-		FastJsonConfig config = new FastJsonConfig();
-		config.setSerializerFeatures(SerializerFeature.DisableCircularReferenceDetect);
-		converter.setFastJsonConfig(config);
-
-		converters.add(0, converter);
-	}
+//	@Override
+//	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//		FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+//
+//		// fast json 循环引用
+//		FastJsonConfig config = new FastJsonConfig();
+//		config.setSerializerFeatures(SerializerFeature.DisableCircularReferenceDetect);
+//		converter.setFastJsonConfig(config);
+//
+//		converters.add(0, converter);
+//	}
 
 }
